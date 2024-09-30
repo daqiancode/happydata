8c36fb52431e30bfd83a7ac61e73fd9330dd8f9a
daqian <daqiancode@gmail.com>
2024-09-30 21:55:11 +0400

    add trim

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup, find_packages
 
 # pip install build twine
 # python -m build
-# python -m twine upload --verbose dist/happydata-0.0.6*
+# python -m twine upload --verbose dist/happydata-0.0.7*
 # pip install happydata
 
 setup(
     name='happydata',
-    version='0.0.6',
+    version='0.0.7',
     author="Daqian",
     packages=find_packages(exclude=['tests', 'tests.*','venv']),
     zip_safe=False,
