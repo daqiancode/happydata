2b29eb8c72320d7076e1234a13d52cdd3f8b4fb7
daqian <daqiancode@gmail.com>
2024-10-10 21:22:15 +0400

    0.0.9

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup, find_packages
 
 # pip install build twine
 # python -m build
-# python -m twine upload --verbose dist/happydata-0.0.8*
+# python -m twine upload --verbose dist/happydata-0.0.9*
 # pip install happydata
 
 setup(
     name='happydata',
-    version='0.0.8',
+    version='0.0.9',
     author="Daqian",
     packages=find_packages(exclude=['tests', 'tests.*','venv']),
     zip_safe=False,
